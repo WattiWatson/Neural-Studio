4fd83d7c8f9fc5c1eb1ce3eaaa51a938300e7e99
Shieldm2023 <77257240+Shieldm2023@users.noreply.github.com>
2024-02-03 18:34:47 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,5 +3,8 @@ from sklearn import datasets
 from matplotlib import pyplot
 import plotly
 import tensorflow as tf
+import iris_example as iris
+
+iris.irisExample(3, [10,10,10],100)
 
 print("Hello!")
